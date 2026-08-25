13ba7b41775ee83c1399eb84c17e008aa13d7a67
merceralex397-collab <merceralex397@gmail.com>
2026-08-25 22:19:52 +0100

    Fix migration expectation for canonical mileage

--- tests/Pegasus.IntegrationTests/IntakePersistenceIntegrationTests.cs
+++ tests/Pegasus.IntegrationTests/IntakePersistenceIntegrationTests.cs
@@ -91,7 +91,8 @@ public sealed class IntakePersistenceIntegrationTests
                 "20260821095500_GrantWorkerVehicleLookupRequests",
                 "20260821100623_GrantImageIntakeLifecycleUpdates",
                 "20260822044425_GrantWorkerCaseDocuments",
-                "20260825122524_DropEvaHandoffProvenanceAndManifest"
+                "20260825122524_DropEvaHandoffProvenanceAndManifest",
+                "20260825202208_CanonicalCaseMileageProvenance"
             ],
             (await context.Database.GetAppliedMigrationsAsync()).ToArray());
         Assert.Empty(await context.Database.GetPendingMigrationsAsync());
